b689aa4bd8d5f85e7c421f4a80d9ca6c2a9fd061
yrarchitechture-wq <yr.architechture@gmail.com>
2026-02-07 21:58:54 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,130 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20260207_211908_134",
+      "name": "IMG_20260207_211908_134",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.25558268957617614,
+        "pitch": -0.008251393887483971,
+        "fov": 1.0818719841848854
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.016004794931442134,
+          "pitch": 0.2283786876842786,
+          "rotation": 0,
+          "target": "1-img_20260207_211920_152"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_20260207_211920_152",
+      "name": "IMG_20260207_211920_152",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0101185071955978,
+          "pitch": 0.19379175930256842,
+          "rotation": 0,
+          "target": "2-img_20260207_211926_466"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-img_20260207_211926_466",
+      "name": "IMG_20260207_211926_466",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
